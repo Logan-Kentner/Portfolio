586fa5a8836aa50d7848a7bd7d6102592d5e12f1
Logan-Kentner <156842486+Logan-Kentner@users.noreply.github.com>
2025-06-06 14:40:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
-console.log('Is this working?');
-
 let viz;
 
-//Add Share Link to Tableau Public in here
+// Tableau public dashboard link
 const url = "https://public.tableau.com/views/PEACEUNSDGRevisited2025/PEACESDG2025?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link";
 
+// DOM elements
 const vizContainer = document.getElementById('vizContainer');
+const launchVizBtn = document.getElementById('launchViz');
+const exportPDF = document.getElementById('exportPDF');
+const exportImage = document.getElementById('exportImage');
+
+// Tableau options
 const options = {
     hideTabs: true,
     height: 1000,
     width: 1200,
     onFirstInteraction: function() {
-        workbook = viz.getWorkbook();
-        activeSheet = workbook.getActiveSheet();
-        console.log("My dashboard is interactive");
+        console.log("Dashboard is interactive");
     }
 };
 
-//create a function to generate the viz element
+// Init viz only on click
 function initViz() {
-    console.log('Executing the initViz function!');
-    viz = new tableau.Viz(vizContainer, url, options);
+    if (!viz) {
+        viz = new tableau.Viz(vizContainer, url, options);
+    }
 }
 
-// run the initViz function when the page loads
-launchVizLink.addEventListener("click", function (e) {
-    e.preventDefault(); // prevent default hyperlink behavior
+// Event listener for Launch button
+launchVizBtn.addEventListener("click", function (e) {
+    e.preventDefault();
     initViz();
 });
 
-//click on the pdf button to generate pdf of dashboard
-function generatePDF() {
-    viz.showExportPDFDialog()
-}
-
+// PDF export
 exportPDF.addEventListener("click", function () {
-    generatePDF();
-  });
+    if (viz) {
+        viz.showExportPDFDialog();
+    }
+});
 
-//click on image to generate image of dashboard
-function generateImage() {
-    viz.showExportImageDialog()
-}
-
+// Image export
 exportImage.addEventListener("click", function () {
-    generateImage();
-  });
+    if (viz) {
+        viz.showExportImageDialog();
+    }
+});
